cf1b4d897dad754cba94062ca9f61e6c54f85c54
RupertDeWeerd <rupert.deweerd@student.ap.be>
2018-05-12 16:29:01 +0200

    FAQ

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,20 @@
 function main() {
-          // hier komt je code
-}
+        var acc = document.getElementsByClassName("question");
+        var i;
+
+        for (i = 0; i < acc.length; i++) {
+                acc[i].addEventListener("click", function() {
+                        this.classList.toggle("active");
+                        var answer = this.nextElementSibling;
+                        if (answer.style.maxHeight){
+                                answer.style.maxHeight = null;
+                        } else {
+                                answer.style.maxHeight = answer.scrollHeight + "rem";
+                        }
+                });
+        }
+}/* END function main() */
 
 window.onload = function() {
-       main();
+        main();
 }
